1cd7b7cc10c1d3b811e5ac7c6169283a40f1a601
ramblersm <cooljishi@gmail.com>
2016-07-13 00:19:40 +0530

    changed NewLanding Page - text and buttons on xaml.

--- visiolite1/visiolite1/NewLandingPage.xaml.cs
+++ visiolite1/visiolite1/NewLandingPage.xaml.cs
@@ -13,66 +13,53 @@ namespace visiolite1
         public NewLandingPage()
         {
             InitializeComponent();
-            Content= new Label
-            {
-                //landing page message
-                FormattedText = new FormattedString
-                {
-                    Spans =
-                    {
-                        new Span
-                        {
-                        Text = "Welcome to",
-                        FontSize=Device.GetNamedSize(NamedSize.Large ,typeof(Label))
-                         },
-                        new Span
-                        {
-                            Text= " Visiolite",
-                             FontSize=Device.GetNamedSize(NamedSize.Large ,typeof(Label)),
-                             FontAttributes=FontAttributes.Bold
-                        }
+            continueButton.Clicked += tapToContinue;
+            //moved the content below to xaml code
+            //Content = new Label
+            //{
+            //    //landing page message
+            //    FormattedText = new FormattedString
+            //    {
+            //        Spans =
+            //        {
+            //            new Span
+            //            {
+            //            Text = "Welcome to",
+            //            FontSize=Device.GetNamedSize(NamedSize.Large ,typeof(Label))
+            //             },
+            //            new Span
+            //            {
+            //                Text= " Visiolite",
+            //                 FontSize=Device.GetNamedSize(NamedSize.Large ,typeof(Label)),
+            //                 FontAttributes=FontAttributes.Bold
+            //            }
 
 
 
-                        //new Span
-                        //  {
-                        //    Text = "\n Tap to Continue"
+            //            //new Span
+            //            //  {
+            //            //    Text = "\n Tap to Continue"
 
-                        //  }
-                    }
+            //            //  }
+            //        }
 
 
                    
 
-                },
+            //    },
 
-               HorizontalTextAlignment = TextAlignment.Center,
-               VerticalTextAlignment = TextAlignment.Center,
-               BackgroundColor = Color.Teal,
-               TextColor = Color.White,
+            //   HorizontalTextAlignment = TextAlignment.Center,
+            //   VerticalTextAlignment = TextAlignment.Center,
+            //   BackgroundColor = Color.Teal,
+            //   TextColor = Color.White,
 
 
-            };
-            //var continueButton = new Button { Text = "Tap to continue" };
-            //continueButton.Clicked += tapToContinue;
+            //};
 
         }
 
-        //public void tapToContinue()
-        //{
-
-        //    Button gotoModalButton = new Button
-        //    {
-        //        Text = "Tap to Continue"
-
-        //    };
-            async void tapToContinue (object sender, EventArgs args)
+                async void tapToContinue (object sender, EventArgs args)
             { 
-  //{
-  //    await Navigation.PopModalAsync();
-  //}
-  //          gotoModalButton.Clicked += async (sender, args) =>
-  //                {
                       await Navigation.PushModalAsync(new SignInPage());
                   }
         
